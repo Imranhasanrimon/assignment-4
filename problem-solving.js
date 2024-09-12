23363f3bac9852bb6f1cb51b69bacf4ff62507db
Imranhasanrimon <imranhasanrimon5@gmail.com>
2024-09-12 15:52:33 +0600

    removing vowels from a sring

--- problem-solving.js
+++ problem-solving.js
@@ -1,17 +1,38 @@
+// Problem 4
 function greetings(name) {
     return `Hello, ${name} How are you doing todat?`
 }
 console.log(greetings("Imran"));
 
-// problem two
+// problem 5
 function whatCentury(year) {
     return Math.ceil(year / 100)
 }
 console.log(whatCentury(2021));
 
-//Problem three
+//Problem 6
 function waterLtr(hours) {
     let perHourWater = 0.5;
     return Math.floor(perHourWater * hours)
 }
 console.log(waterLtr(12.3));
+
+//Problem 7
+function isDivisible(n, x, y) {
+    if (n % x === 0 && n % y === 0) {
+        return true;
+    } else return false;
+}
+console.log(isDivisible(12, 3, 4));
+
+//Problem 9
+function disEmvowel(string) {
+    let newString = ''
+    for (let i of string) {
+        if (!'aeiou'.includes(i.toLowerCase())) {
+            newString += i;
+        }
+    }
+    return newString
+}
+console.log(disEmvowel('This website is for losers LOL!'));
