46e168592a75fe5d902a319eeba4bd8dc473c211
Imranhasanrimon <imranhasanrimon5@gmail.com>
2024-09-12 09:44:55 +0600

    solving a problem

--- practice.js
+++ practice.js
@@ -136,3 +136,23 @@ function checkDigitsInName(name) {
     return output;
 
 }
+
+function waitingTime(waitingTimes, serialNumber) {
+
+    if (!Array.isArray(waitingTimes) || typeof serialNumber !== 'number' || waitingTimes.length <= 0) {
+        return "Invalid Input";
+    }
+
+    let totalwaitingTimes = 0;
+
+    for (let i of waitingTimes) {
+        totalwaitingTimes += i;
+    }
+
+    const avgTime = Math.round(totalwaitingTimes / waitingTimes.length);
+    const mySerial = serialNumber - 1;
+    const remainingCandidate = mySerial - waitingTimes.length
+    const requiredTime = avgTime * remainingCandidate;
+
+    return requiredTime;
+}
