a5309f42cf1101aab85186a36fc35c3a1088a2d2
Imranhasanrimon <imranhasanrimon5@gmail.com>
2024-09-12 15:31:29 +0600

    solving water related problem

--- problem-solving.js
+++ problem-solving.js
@@ -2,3 +2,16 @@ function greetings(name) {
     return `Hello, ${name} How are you doing todat?`
 }
 console.log(greetings("Imran"));
+
+// problem two
+function whatCentury(year) {
+    return Math.ceil(year / 100)
+}
+console.log(whatCentury(2021));
+
+//Problem three
+function waterLtr(hours) {
+    let perHourWater = 0.5;
+    return Math.floor(perHourWater * hours)
+}
+console.log(waterLtr(12.3));
